89b6747d5c54c6e0ec7ad0962ba3309b6c41bd47
Steven Schwenke <steven@stevenschwenke.de>
2018-02-14 20:36:47 +0100

    Add completely new, custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import {ServerComponent} from "./server/server.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
