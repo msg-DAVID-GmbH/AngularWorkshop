e77749c2afcf57c63b68a591a5335b414353baac
Steven Schwenke <steven@stevenschwenke.de>
2018-02-18 14:37:04 +0100

    Add new component "servers" (created via CLI)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
